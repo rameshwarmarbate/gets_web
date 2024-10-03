5ed60b70a43de9aa3333b956e01064a514a86784
Rameshwar <tnb@vspacesoft.com>
2024-10-03 22:09:56 +0530

    bug fixed

--- src/router.jsx
+++ src/router.jsx
@@ -1,17 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/login/Login";
 import OrderList from "./pages/order/OrderList";
 import ResetPassword from "./pages/login/ResetPassword";
 import { isUserAuthenticated } from "./utils/helpers";
+import AuthGuard from "./context/AuthGuard";
 
 const Navigation = () => {
   const isLoggedIn = isUserAuthenticated();
   return (
     <Router>
       <Routes>
-        <Route path="/" element={!isLoggedIn ? <Login /> : <OrderList />} />
+        <Route
+          path="/"
+          element={
+            !isLoggedIn ? <Login /> : <Navigate replace to={"/orders"} />
+          }
+        />
         <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/orders" element={<OrderList />} />
+        <Route
+          path="/orders"
+          element={
+            <AuthGuard>
+              <OrderList />
+            </AuthGuard>
+          }
+        />
       </Routes>
     </Router>
   );
