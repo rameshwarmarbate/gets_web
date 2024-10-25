677182b6a63a842d1ab3c018c3c5b86f99f145b7
rameshwarmarbate <rameshwarmarbate@gmail.com>
2024-10-25 11:38:03 +0530

    add order changes

--- src/pages/order/AddOrder.jsx
+++ src/pages/order/AddOrder.jsx
@@ -1,118 +1,367 @@
-import React, { useState } from 'react';
-import { Modal, ModalDialog, DialogTitle, DialogContent, DialogActions } from '@mui/joy';
-import { Box, TextField, Select, Option, Button, Typography, Divider } from '@mui/joy';
+import React, { useEffect, useState } from "react";
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Divider,
+  FormControl,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Loader } from "../../components";
+import { useGetData } from "../../utils/api";
+
+const initialStates = {
+  firstName: "",
+  lastName: "",
+  mobileNo: "",
+  email: "",
+  address: "",
+  pinCode: "",
+  city: "",
+  state: "",
+  country: "",
+  selectedProduct: "",
+  quantity: "",
+  unitPrice: "",
+  gstNo: "",
+};
+const fetchLocationByPinCode = async (pinCode) => {
+  try {
+    const response = await fetch(`http://api.zippopotam.us/in/${pinCode}`);
+    if (!response.ok) throw new Error("Network response was not ok");
+    const data = await response.json();
+    return {
+      city: data.places ? data.places[0]["place name"] : "",
+      state: data.places ? data.places[0]["state"] : "",
+      country: data["country"] || "",
+    };
+  } catch (error) {
+    console.error("Error fetching location data:", error);
+    return null;
+  }
+};
 
 export default function AddOrderModal({ open, handleClose }) {
-  const [customerData, setCustomerData] = useState({
-    firstName: '',
-    lastName: '',
-    mobileNo: '',
-    email: '',
-    address: '',
-    pinCode: ''
-  });
+  const [orderData, setOrderData] = useState(initialStates);
 
-  const [productData, setProductData] = useState({
-    selectedProduct: '',
-    quantity: '',
-    unitPrice: ''
-  });
+  const [error, setErrors] = useState(initialStates);
+  const [isLoading, setLoading] = useState(false);
+  const { data, isLoading: loading } = useGetData(
+    "device/get-device-by-name?name=Electric Hot Water Bag"
+  );
+  useEffect(() => {
+    const { pinCode } = orderData;
+    if (pinCode.length === 6) {
+      setLoading(true);
+      fetchLocationByPinCode(pinCode).then((data) => {
+        if (data) {
+          setOrderData((prevState) => ({
+            ...prevState,
+            city: data.city || "",
+            state: data.state || "",
+            country: data.country || "",
+          }));
+        }
+        setLoading(false);
+      });
+    }
+  }, [orderData.pinCode]);
 
-  const handleCustomerChange = (e) => {
+  const handleOrderChange = (e) => {
     const { name, value } = e.target;
-    setCustomerData({ ...customerData, [name]: value });
-  };
-
-  const handleProductChange = (e) => {
-    const { name, value } = e.target;
-    setProductData({ ...productData, [name]: value });
+    setOrderData((prevState) => ({ ...prevState, [name]: value }));
   };
 
   const handleSubmit = () => {
     // Handle form submission logic here
-    console.log({ customerData, productData });
+    console.log({ orderData });
     handleClose(); // Close the modal after submission
   };
-
   return (
-    <Modal open={open} onClose={handleClose}>
-      <ModalDialog variant="outlined" role="dialog">
-        <DialogTitle>Add Order</DialogTitle>
-        <Divider />
-        <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-          <Typography level="h6">Customer Details</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+    <Dialog open={open} onClose={handleClose} fullWidth>
+      <DialogTitle>Add Order</DialogTitle>
+      <Divider />
+      <DialogContent
+        sx={{ display: "flex", flexDirection: "column", gap: 2, width: "100%" }}
+      >
+        <Loader loading={isLoading || loading} />
+        <Typography level="h6">Customer Details</Typography>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+          <FormControl>
             <TextField
-              label="First Name"
+              id="first-name"
+              required
+              autoFocus
+              autoComplete="first-name"
+              size="small"
+              margin="dense"
               name="firstName"
-              value={customerData.firstName}
-              onChange={handleCustomerChange}
+              label="First Name"
+              placeholder="First Name"
+              type="text"
+              fullWidth
+              error={!!error.firstName}
+              helperText={error.firstName}
+              sx={{ ariaLabel: "first-name" }}
+              color={error.firstName ? "error" : "primary"}
+              value={orderData.firstName}
+              onChange={handleOrderChange}
             />
+          </FormControl>
+          <FormControl>
             <TextField
-              label="Last Name"
+              id="last-name"
+              required
+              autoComplete="last-name"
+              size="small"
+              margin="dense"
               name="lastName"
-              value={customerData.lastName}
-              onChange={handleCustomerChange}
+              label="Last Name"
+              placeholder="Last Name"
+              type="text"
+              fullWidth
+              error={!!error.lastName}
+              helperText={error.lastName}
+              sx={{ ariaLabel: "last-name" }}
+              color={error.lastName ? "error" : "primary"}
+              value={orderData.lastName}
+              onChange={handleOrderChange}
             />
+          </FormControl>
+          <FormControl>
             <TextField
-              label="Mobile No"
+              id="mobile-number"
+              required
+              autoComplete="mobile-number"
+              size="small"
+              margin="dense"
               name="mobileNo"
-              value={customerData.mobileNo}
-              onChange={handleCustomerChange}
+              label="Mobile No"
+              placeholder="Mobile No"
+              type="tel"
+              fullWidth
+              error={!!error.mobileNo}
+              helperText={error.mobileNo}
+              sx={{ ariaLabel: "mobile-number" }}
+              color={error.mobileNo ? "error" : "primary"}
+              value={orderData.mobileNo}
+              inputMode="tel"
+              onChange={handleOrderChange}
             />
+          </FormControl>
+          <FormControl>
             <TextField
-              label="Email ID"
+              id="email"
+              autoComplete="email"
+              size="small"
+              margin="dense"
               name="email"
-              value={customerData.email}
-              onChange={handleCustomerChange}
+              label="Email Id"
+              placeholder="Email Id"
+              type="email"
+              fullWidth
+              error={!!error.email}
+              helperText={error.email}
+              sx={{ ariaLabel: "email" }}
+              color={error.email ? "error" : "primary"}
+              value={orderData.email}
+              onChange={handleOrderChange}
             />
+          </FormControl>
+          <FormControl>
             <TextField
-              label="Address"
+              id="address"
+              autoComplete="address"
+              size="small"
+              margin="dense"
               name="address"
-              value={customerData.address}
-              onChange={handleCustomerChange}
+              label="Address"
+              placeholder="Address"
+              type="text"
+              rows={2}
+              fullWidth
+              error={!!error.address}
+              helperText={error.address}
+              sx={{ ariaLabel: "address" }}
+              color={error.address ? "error" : "primary"}
+              value={orderData.address}
+              onChange={handleOrderChange}
             />
+          </FormControl>
+          <FormControl>
             <TextField
-              label="Pin Code"
+              id="pin-code"
+              autoComplete="pin-code"
+              size="small"
+              margin="dense"
               name="pinCode"
-              value={customerData.pinCode}
-              onChange={handleCustomerChange}
+              label="Pin Code"
+              placeholder="Pin Code"
+              type="number"
+              fullWidth
+              error={!!error.pinCode}
+              helperText={error.pinCode}
+              sx={{ ariaLabel: "pin-code" }}
+              color={error.pinCode ? "error" : "primary"}
+              value={orderData.pinCode}
+              inputMode="numeric"
+              onChange={handleOrderChange}
             />
-          </Box>
-
-          <Typography level="h6">Product Details</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-            <Select
-              label="Select Product"
-              name="selectedProduct"
-              value={productData.selectedProduct}
-              onChange={handleProductChange}
-            >
-              <Option value="Product1">Product 1</Option>
-              <Option value="Product2">Product 2</Option>
-              <Option value="Product3">Product 3</Option>
-            </Select>
+          </FormControl>
+          <FormControl>
             <TextField
-              label="Quantity"
+              id="city"
+              autoComplete="city"
+              size="small"
+              margin="dense"
+              name="city"
+              label="City"
+              placeholder="City"
+              type="text"
+              fullWidth
+              error={!!error.city}
+              helperText={error.city}
+              sx={{ ariaLabel: "city" }}
+              color={error.city ? "error" : "primary"}
+              value={orderData.city}
+              onChange={handleOrderChange}
+            />
+          </FormControl>
+          <FormControl>
+            <TextField
+              id="state"
+              autoComplete="state"
+              size="small"
+              margin="dense"
+              name="state"
+              label="State"
+              placeholder="State"
+              type="text"
+              fullWidth
+              error={!!error.state}
+              helperText={error.state}
+              sx={{ ariaLabel: "state" }}
+              color={error.state ? "error" : "primary"}
+              value={orderData.state}
+              onChange={handleOrderChange}
+            />
+          </FormControl>
+          <FormControl>
+            <TextField
+              id="country"
+              autoComplete="country"
+              size="small"
+              margin="dense"
+              name="country"
+              label="Country"
+              placeholder="Country"
+              type="text"
+              fullWidth
+              error={!!error.country}
+              helperText={error.country}
+              sx={{ ariaLabel: "country" }}
+              color={error.country ? "error" : "primary"}
+              value={orderData.country}
+              onChange={handleOrderChange}
+            />
+          </FormControl>
+          <FormControl>
+            <TextField
+              id="gstNo"
+              autoComplete="gstNo"
+              size="small"
+              margin="dense"
+              name="gstNo"
+              label="GST No"
+              placeholder="GST No"
+              type="text"
+              fullWidth
+              error={!!error.gstNo}
+              helperText={error.gstNo}
+              sx={{ ariaLabel: "gstNo" }}
+              color={error.gstNo ? "error" : "primary"}
+              value={orderData.gstNo}
+              onChange={handleOrderChange}
+            />
+          </FormControl>
+        </Box>
+        <Typography level="h6">Product Details</Typography>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+          <Autocomplete
+            id="product"
+            name="selectedProduct"
+            size="small"
+            autoComplete
+            autoSelect
+            fullWidth
+            options={data}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Select Product"
+                placeholder="Select Product"
+                value={orderData.selectedProduct}
+                onChange={handleOrderChange}
+              />
+            )}
+            getOptionLabel={(option) => option.title}
+          />
+          <FormControl>
+            <TextField
+              id="quantity"
+              autoComplete="quantity"
+              size="small"
+              margin="dense"
               name="quantity"
+              label="Quantity"
+              placeholder="Quantity"
               type="number"
-              value={productData.quantity}
-              onChange={handleProductChange}
+              fullWidth
+              error={!!error.quantity}
+              helperText={error.quantity}
+              sx={{ ariaLabel: "quantity" }}
+              color={error.quantity ? "error" : "primary"}
+              value={orderData.quantity}
+              inputMode="numeric"
+              onChange={handleOrderChange}
             />
+          </FormControl>
+          <FormControl>
             <TextField
-              label="Unit Price"
+              id="unit-price"
+              autoComplete="unit-price"
+              size="small"
+              margin="dense"
               name="unitPrice"
+              label="Unit Price"
+              placeholder="Unit Price"
               type="number"
-              value={productData.unitPrice}
-              onChange={handleProductChange}
+              fullWidth
+              inputMode="decimal"
+              error={!!error.unitPrice}
+              helperText={error.unitPrice}
+              sx={{ ariaLabel: "unit-price" }}
+              color={error.unitPrice ? "error" : "primary"}
+              value={orderData.unitPrice}
+              onChange={handleOrderChange}
             />
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button variant="contained" onClick={handleSubmit}>Save</Button>
-        </DialogActions>
-      </ModalDialog>
-    </Modal>
+          </FormControl>
+        </Box>
+      </DialogContent>
+      <DialogActions sx={{ pb: 3, px: 3 }}>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button variant="contained" onClick={handleSubmit}>
+          Save
+        </Button>
+        <Button variant="contained" onClick={handleSubmit} disabled>
+          Save & Whatsapp
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
--- src/pages/order/OrderList.jsx
+++ src/pages/order/OrderList.jsx
@@ -15,7 +15,6 @@ import OrderList from "../../components/OrderList";
 import Header from "../../components/Header";
 import { AddCircleOutlineRounded } from "@mui/icons-material";
 import AddOrder from "./AddOrder";
-import { Modal } from "@mui/joy";
 
 export default function OrderDashboardTemplate() {
   const [open, setOpen] = useState(false);
@@ -29,78 +28,78 @@ export default function OrderDashboardTemplate() {
   };
 
   return (
-    <CssVarsProvider disableTransitionOnChange>
-      <CssBaseline />
-      <Box sx={{ display: "flex", minHeight: "100dvh" }}>
-        <Header />
-        <Sidebar />
-        <Box
-          component="main"
-          className="MainContent"
-          sx={{
-            px: { xs: 2, md: 6 },
-            pt: {
-              xs: "calc(12px + var(--Header-height))",
-              sm: "calc(12px + var(--Header-height))",
-              md: 3,
-            },
-            pb: { xs: 2, sm: 2, md: 3 },
-            flex: 1,
-            display: "flex",
-            flexDirection: "column",
-            minWidth: 0,
-            height: "100dvh",
-            gap: 1,
-          }}
-        >
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <Breadcrumbs
-              size="sm"
-              aria-label="breadcrumbs"
-              separator={<ChevronRightRoundedIcon fontSize="sm" />}
-              sx={{ pl: 0 }}
-            >
-              <Link underline="none" color="neutral" aria-label="Home">
-                <HomeRoundedIcon />
-              </Link>
-              <Typography
-                color="primary"
-                sx={{ fontWeight: 500, fontSize: 12 }}
-              >
-                Orders
-              </Typography>
-            </Breadcrumbs>
-          </Box>
+    <>
+      <CssVarsProvider disableTransitionOnChange>
+        <CssBaseline />
+        <Box sx={{ display: "flex", minHeight: "100dvh" }}>
+          <Header />
+          <Sidebar />
           <Box
+            component="main"
+            className="MainContent"
             sx={{
+              px: { xs: 2, md: 6 },
+              pt: {
+                xs: "calc(12px + var(--Header-height))",
+                sm: "calc(12px + var(--Header-height))",
+                md: 3,
+              },
+              pb: { xs: 2, sm: 2, md: 3 },
+              flex: 1,
               display: "flex",
-              mb: 1,
+              flexDirection: "column",
+              minWidth: 0,
+              height: "100dvh",
               gap: 1,
-              flexDirection: { xs: "column", sm: "row" },
-              alignItems: { xs: "start", sm: "center" },
-              flexWrap: "wrap",
-              justifyContent: "space-between",
             }}
           >
-            <Typography level="h2" component="h1">
-              Orders
-            </Typography>
-            <Button
-              color="primary"
-              startDecorator={<AddCircleOutlineRounded />}
-              size="sm"
-              onClick={handleClickOpen}
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Breadcrumbs
+                size="sm"
+                aria-label="breadcrumbs"
+                separator={<ChevronRightRoundedIcon fontSize="sm" />}
+                sx={{ pl: 0 }}
+              >
+                <Link underline="none" color="neutral" aria-label="Home">
+                  <HomeRoundedIcon />
+                </Link>
+                <Typography
+                  color="primary"
+                  sx={{ fontWeight: 500, fontSize: 12 }}
+                >
+                  Orders
+                </Typography>
+              </Breadcrumbs>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                mb: 1,
+                gap: 1,
+                flexDirection: { xs: "column", sm: "row" },
+                alignItems: { xs: "start", sm: "center" },
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+              }}
             >
-              Add Order
-            </Button>
+              <Typography level="h2" component="h1">
+                Orders
+              </Typography>
+              <Button
+                color="primary"
+                startDecorator={<AddCircleOutlineRounded />}
+                size="sm"
+                onClick={handleClickOpen}
+              >
+                Add Order
+              </Button>
+            </Box>
+            <OrderTable />
+            <OrderList />
           </Box>
-          <OrderTable />
-          <OrderList />
-          <Modal open={open} onClose={handleClose}>
-            <AddOrder open={open} handleClose={handleClose} />
-          </Modal>
         </Box>
-      </Box>
-    </CssVarsProvider>
+      </CssVarsProvider>
+      {open ? <AddOrder open={open} handleClose={handleClose} /> : null}
+    </>
   );
 }
